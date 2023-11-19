0db26b878f314672f6dbe2425123ba768cae3256
= <=>
2023-11-19 01:58:57 -0800

    implemented preliminary border calc

--- helpers.py
+++ helpers.py
@@ -1,4 +1,7 @@
 import os
+import math
+
+import json
 
 import numpy as np
 import cv2
@@ -56,10 +59,28 @@ def scale_border_coords(raw_coords: dict[str, tuple[int, int]], scale: int) -> d
 
 def get_border_coords(img_path: str, scale: int) -> dict[str, int]:
     """
-    combines the 2 function above to get left, right up, down border
+    Once the user set up a folder with 4 images (named L/U/R/D.jpg), pass the
+    folder as img_path to set border
+
+    img_path: Path to the 4 border images
+    scale: scale to view the images at
+
+    returns - Dictionary with 4 elements L, U, R, D, each referencing a pixel
+    position for the respective side
     """
     rel_coords = find_border(img_path, scale)
     return scale_border_coords(rel_coords, 1/scale)
+
+
+def store_coords(coords: dict[str, int], store_path: str = os.getcwd()) -> None:
+    """
+    stores coordinates of border into json file for reference during gameplay
+    """
+    if not os.path.exists(store_path):
+        os.makedirs(store_path)
+
+    with open(os.path.join(store_path, "border.json"), 'w') as file:
+        json.dump(coords, file)
 
 
 def draw_border(image: np.ndarray, coords: dict[str, int], color: tuple[int, int, int], thickness: int=10):
@@ -92,3 +113,86 @@ def view_img_with_border(img_path: str, coords: dict[str, int], scale: int) -> N
     cv2.imshow("bordered", img)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
+
+
+
+def calculate_trajectory(start_pos: tuple[int, int], shot_angle: int, border: dict[str, int], reflect_limit: int = 10) -> list[tuple[int, int]]:
+    """
+    start_x, start_y: starting position for current unit
+    shot_angle: angle of shot in degrees (0-359), 0 referencing --> direction
+    border: location of border, dict[str, int]
+    reflections: number of reflections to compute, e.g. 1 means stop when hit 1 wall
+    """
+
+    #preset values
+    x, y = start_pos
+    angle = shot_angle * math.pi/180
+    step = 0.1 #step size to take in given direction in pixel
+
+    #calculate process
+    points: list[tuple[int, int]] = [start_pos]
+
+    while len(points) <= reflect_limit+1:
+        # Calculate next position based on current_angle
+        x += step*math.cos(angle)
+        y += step*math.sin(angle)
+
+        # Check if the new position hits any border
+        border_hit = check_border_hit(x, y, border)
+        if border_hit:
+            angle = get_reflected_angle(angle, border_hit)
+            points.append((round(x), round(y)))
+
+    return points
+
+def check_border_hit(x: float, y: float, border: tuple[str, int]) -> str:
+    """
+    First check for 2 border cases, then check for single border hit.
+    
+    Returns: empty string if no border hit, '2' if 2 borders hit, or otherwise the
+    corresponding border hit.
+    """
+    border_hit = ""
+    num_hit = 0
+
+    if x < border["L"]:
+        border_hit = "L"
+        num_hit += 1
+
+    if x > border["R"]:
+        border_hit = "R"
+        num_hit += 1
+
+    if y < border["U"]:
+        border_hit = "U"
+        num_hit += 1
+
+    if y > border["D"]:
+        border_hit = "D"
+        num_hit += 1
+
+    if num_hit == 2:
+        border_hit = "2"
+
+
+    if num_hit > 2:
+        raise ValueError("Error, more than 2 borders hit at the same time")
+
+    return border_hit
+
+def get_reflected_angle(angle: float, border_hit: str):
+    """
+    for why these angles work, see:
+    https://math.libretexts.org/Bookshelves/Precalculus/Elementary_Trigonometry_(Corral)/01%3A_Right_Triangle_Trigonometry_Angles/1.05%3A_Rotations_and_Reflections_of_Angles
+
+    where instead of a simple reflection on incident axis, we reflect its opposite direction path through the other axis
+    """
+    if border_hit in {"L", "R"}:
+        return math.pi-angle
+    
+    if border_hit in {"U", "D"}:
+        return -angle
+    
+    if border_hit == "2":
+        return angle + math.pi
+    
--- main.py
+++ main.py
@@ -1,9 +1,22 @@
-from helpers import get_border_coords, view_img_with_border
+import json
+from helpers import get_border_coords, view_img_with_border, store_coords, calculate_trajectory
 
 
-with open("logs.txt", "w"):
-    pass
+#for now, please set this flag to let the program know if your border is set.
+border_set = True
 
-coords = get_border_coords("imgs", scale= 1/2.5)
+while not border_set:
+    coords = get_border_coords("imgs", scale= 1/2.5)
 
-view_img_with_border("imgs/R.jpg", coords, scale = 1/2.5)
+    view_img_with_border("imgs/R.jpg", coords, scale = 1/2.5)
+
+    confirm_border = input("Is the border set correctly? (y/n): ")
+
+    if confirm_border == "y":
+        border_set = True
+        store_coords(coords)
+
+
+with open("border.json", "r") as border_file:
+    border = json.load(border_file)
+    print(calculate_trajectory((500, 500), 10, border))
